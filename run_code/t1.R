0992b992616da2373378e79830079b704c6c94ca
Peter Winskill <p.winskill@imperial.ac.uk>
2018-08-16 14:48:51 +0100

    Test running

--- run_code/t1.R
+++ run_code/t1.R
@@ -1,8 +1,9 @@
 devtools::load_all()
 
-system.time(t1 <- demog_test(500000, 365 * 50, 1, age_dist, prob_death))
+system.time(t1 <- demog_test(500000, 365 * 200, 1, age_dist, prob_death))
 
 tcut <- rep(1:(200*5), each = 365/5)
 ann_Death <- tapply(t1, tcut, sum)
 plot(ann_Death)
 plot(t1, t = "l")
+
