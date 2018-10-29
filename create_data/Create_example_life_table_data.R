120d9f3ce5fd90fbe482251e2e990fde03b3e438
Peter Winskill <p.winskill@imperial.ac.uk>
2018-10-29 20:13:11 +0000

    Smoothing spline on life table data

--- create_data/Create_example_life_table_data.R
+++ create_data/Create_example_life_table_data.R
@@ -3,15 +3,32 @@
 mali_life_table <- read.csv("create_data/Mali_life_table_example.csv")
 
 prob_death <- mali_life_table$P_alive
-prob_death <- prob_death / sum(prob_death)
+prob_death1 <- prob_death / sum(prob_death)
 
 reps <- c(1, 4, rep(5, 17))
 age_cat <- 0:89
 
-prob_death <- rep(prob_death, reps)
+# Fitting smoothing spline for death probability at any age
+library(dplyr)
+
+sm <- mali_life_table %>%
+  mutate(p = P_alive / sum(P_alive),
+         y = Start_year + reps / 2) %>%
+  select(-Start_year, -P_alive)
+
+sf1 <- smooth.spline(sm$y, sm$p)
+prob_death <- predict(sf1, age_cat)$y
+plot(p1 ~ age_cat)
+points(sm$p ~ sm$y, col = "green", pch  = 19)
+
+# Normalise
+prob_death = prob_death / sum(prob_death)
+par(mfrow = c(1, 2))
+plot(prob_death)
+plot(cumsum(prob_death))
 
 # Equilibrium age distribution (estimation)
-N <- 100000
+N <- 1000000
 age <- rep(0, N)
 death <- sample(0:89, N, prob = prob_death, replace = TRUE)
 for(i in 0:1000){
@@ -29,3 +46,18 @@ barplot(prob_death, names.arg = age_cat)
 
 devtools::use_data(prob_death, overwrite = TRUE)
 devtools::use_data(age_dist, overwrite = TRUE)
+
+
+# Fitting smoothing spline for death probability at any age
+library(dplyr)
+
+sm <- mali_life_table %>%
+  mutate(p = P_alive / sum(P_alive),
+         y = Start_year + reps / 2) %>%
+  select(-Start_year, -P_alive)
+
+sf1 <- smooth.spline(sm$y, sm$p)
+x <- seq(0,90,0.5)
+p1 <- predict(sf1, x)$y
+plot(p1 ~ x)
+points(sm$p ~ sm$y, col = "green", pch  = 19)
