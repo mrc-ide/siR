d1e58da1ca2bd26285e2d684af9077fc7e3ebf5a
Peter Winskill <p.winskill@imperial.ac.uk>
2018-12-12 15:05:33 +0000

    Update documentation

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,8 +2,8 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #' @export
-demog_test <- function(N, days, substep, age_distribution, life_distribution) {
-    .Call('_siR_demog_test', PACKAGE = 'siR', N, days, substep, age_distribution, life_distribution)
+demog_test <- function(N, days, substep, age_of_death, birth_times, death_times) {
+    .Call('_siR_demog_test', PACKAGE = 'siR', N, days, substep, age_of_death, birth_times, death_times)
 }
 
 #' @export
