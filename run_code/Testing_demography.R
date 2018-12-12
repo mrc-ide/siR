e784a8e93e3afc6f56c7e875ebc3dd4b3df942a2
Peter Winskill <p.winskill@imperial.ac.uk>
2018-12-12 15:05:12 +0000

    Test new demog implementation

--- run_code/Testing_demography.R
+++ run_code/Testing_demography.R
@@ -5,7 +5,7 @@ library(ggplot2)
 N = 1000000
 days = 36500
 
-t1 <- demog_test(N, days, 10, age_dist_ana, prob_death)
+system.time(demog_test(N, days, 10, age_dist_ana, prob_death))
 
 bin <- rep(1:365, each = 1000)
 out <- data.frame(deaths = t1, bin = bin) %>%
