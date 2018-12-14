b1911ab8a019fc95cd6b4efbdffc16ea25dfeea4
Peter Winskill <p.winskill@imperial.ac.uk>
2018-12-14 09:29:10 +0000

    Complete refactor person class (simplified)

--- src/person.cpp
+++ src/person.cpp
@@ -1,22 +1,12 @@
 #include <Rcpp.h>
 #include "person.h"
-#include "helper.h"
+#include "sample.h"
 #include "string.h"
 #include "time.h"
 
-Person::Person(int &t, int &substep, double &prop_f, std::vector<double> &age_dist, std::vector<double> &prob_death){
-  // Birth time
-  int year = weighted_sample(age_dist, 0);
-  int day = int(R::runif(0, 365));
-  int age =  (year * 365) + day;
-  birth_time = t - days_to_steps(age, substep);
-
-  // Death time
-  int death_year = weighted_sample(prob_death, year);
-  death_time = birth_time + years_to_steps(year, substep);
-  // Sex
-  sex = Person::attribute_sex(prop_f);
-  heterogeneity = 1.0;
+Person::Person(int birth_t, int death_t){
+  birth = birth_t;
+  death = death_t;
 }
 
 char Person::attribute_sex(double prop_f){
@@ -27,18 +17,5 @@ char Person::attribute_sex(double prop_f){
   return s;
 }
 
-void Person::new_birth(int &t, int &substep, double &prop_f, std::vector<double> &prob_death){
-  birth_time = t;
-  int year = weighted_sample(prob_death, 0);
-  int day = int(R::runif(0, 365));
-  int lifespan = (year * 365) + day;
-  death_time = birth_time + days_to_steps(lifespan, substep);
-  sex = Person::attribute_sex(prop_f);
-  heterogeneity = 1.0;
-}
 
-Person2::Person2(int birth_t, int death_t){
-  birth = birth_t;
-  death = death_t;
-}
 
--- src/person.h
+++ src/person.h
@@ -1,30 +1,13 @@
 #ifndef person_h
 #define person_h
 
-
 class Person{
-  public:
-    // Default constructor
-    Person(int &t, int &substep, double &prop_f, std::vector<double> &age_dist, std::vector<double> &prob_death);
-
-    // New birth
-    void new_birth(int &t, int &substep, double &prop_f, std::vector<double> &prob_death);
-
-    // Draw sex of individual
-    char attribute_sex(double prop_f = 0.5);
-
-    // Attributes
-    int birth_time;
-    int death_time;
-    char sex;
-    double heterogeneity;
-
-};
-
-class Person2{
 public:
   // Default constructor
-  Person2(int birth_t, int death_t);
+  Person(int birth_t, int death_t);
+
+  // Draw sex of individual
+  char attribute_sex(double prop_f = 0.5);
 
   // Attributes
   int birth;
