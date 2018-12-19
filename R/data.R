863fc78917a5593db016a80fb3bf1aaf06873f68
Peter Winskill <p.winskill@imperial.ac.uk>
2018-12-19 09:06:15 +0000

    Data documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Probability death with be at a given age.
+#'
+#' Estimated from Mali lifetable data.
+#'
+#' @format A vector with 90 probabilities.
+#' @source See create lifetable data for more information
+"age_of_death"
+
+#' Equilibrium age distribution.
+#'
+#' Estimated from Mali lifetable data.
+#'
+#' @format A vector with 90 probabilities.
+#' @source See create lifetable data for more information
+"equilibrium_age"
