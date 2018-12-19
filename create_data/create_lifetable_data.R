db1a1d9cd1467de2e5123ce129fbf3da72a5b8c5
Peter Winskill <p.winskill@imperial.ac.uk>
2018-12-19 09:08:05 +0000

    Debug creating lifetable data in R equilbrium

--- create_data/create_lifetable_data.R
+++ create_data/create_lifetable_data.R
@@ -9,11 +9,11 @@ smooth_life_table <- prob_death_who_to_annual(life_table)
 # Estimate probability fo death in each age group
 age_of_death <- equilibrium_age_death(smooth_life_table)
 # Estimate equilbrium age distribution
-equilbrium_age <- equilibrium_age_distribution(smooth_life_table)
+equilibrium_age <- equilibrium_age_distribution(smooth_life_table)
 # View outputs
 par(mfrow = c(1, 2))
 plot(age_of_death, ylab = "Probability", main = "Age of death", pch = 19)
-plot(equilbrium_age, ylab = "Probability", main = "Age distribution", pch = 19)
+plot(equilibrium_age, ylab = "Probability", main = "Age distribution", pch = 19)
 #Save data for use in package
 devtools::use_data(age_of_death)
-devtools::use_data(equilbrium_age)
+devtools::use_data(equilibrium_age)
