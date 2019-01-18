93f43bf98f565886417fd7323401d468eda33c17
Peter Winskill <p.winskill@imperial.ac.uk>
2019-01-18 12:32:27 +0000

    Fix equilibrium age testing

--- tests/testthat/test-conditional_age.R
+++ tests/testthat/test-conditional_age.R
@@ -4,7 +4,7 @@ test_that("Equilibrium death age", {
   set.seed(100)
   expect_equal(draw_equilibrium_death_age(2000, 0:89, age_of_death, 89), 22360)
   expect_equal(draw_equilibrium_death_age(0, 0:89, age_of_death, 89), 25571)
-  expect_equal(draw_equilibrium_death_age(90 * 365 - 1, 0:89, age_of_death, 89), 365 * 90 -1)
+  expect_equal(draw_equilibrium_death_age(90 * 365 - 1, 0:89, age_of_death, 89), 365 * 90)
   expect_error(draw_equilibrium_death_age(90 * 365, 0:89, age_of_death, 89))
 })
 
